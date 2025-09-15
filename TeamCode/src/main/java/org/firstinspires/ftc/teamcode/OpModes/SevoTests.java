e7f69f89296073c4630716b9454dcd613f7dc72a
CarlosSeijasFTC <cseijas28@cathedral-elpaso.org>
2025-09-15 11:01:35 -0600

    Update SevoTests.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/SevoTests.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpModes/SevoTests.java
@@ -5,7 +5,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.mechanisms.DevicesForCompetitionServos;
 
-@TeleOp(name = "Test Servos Using Controller")
+@TeleOp(name = "Test Servos Using Controller", group = "Tests")
 public class SevoTests extends OpMode {
 
     boolean wasA;
