c6e401e0a546ab05fc8ee74d736deb8a5bf1ccc9
robot <cseijas28@cathedral-elpaso.org>
2025-09-25 16:35:08 -0600

    9/25/2025

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/MotorMethods.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Methods/MotorMethods.java
@@ -1,10 +1,13 @@
 package org.firstinspires.ftc.teamcode.Methods;
 
+import com.qualcomm.hardware.lynx.LynxModule;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.EmbeddedControlHubModule;
 import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.Range;
 
 public class MotorMethods {
+
     public void MoveToPosition(DcMotor motor, int targetPos, double k_p, double k_i, double k_d){
         ElapsedTime time = new ElapsedTime();
         int accuracy = 2;
@@ -13,6 +16,7 @@ public class MotorMethods {
         double max_i = 0.2, min_i = -0.2;
         double power;
         while (Math.abs(motor.getCurrentPosition()-targetPos) > accuracy){
+
             double currentTime = time.milliseconds();
             double error = targetPos - motor.getCurrentPosition();
 
